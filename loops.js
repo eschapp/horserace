9b219dd308d295047d348c2f777477065745c14f
eschapp <eschapp@gmail.com>
2016-06-21 12:28:56 -0500

    added loop types

--- loops.js
+++ loops.js
@@ -19,12 +19,49 @@ var stable = {
   ]
 };
 
-function feed(horses) {
-  var numHorses = horses.length;
-  for (var i = 0; i < numHorses ; i++ ){
-      console.log(horses[i].name + ": thanks for feeding me!!!!");
+//For Loop
+  // function feed(horses) {
+  //   var numHorses = horses.length;
+  //   for (var i = 0; i < numHorses ; i++ ){
+  //       console.log(horses[i].name + ": thanks for feeding me!!!!");
 
-  }
-}
+  //   }
+  //   console.log("Done feeding!")
+  // }
 
-feed(stable.horses);
+  // feed(stable.horses);
+//
+//
+//Map Loop
+  // function brush(horses) {
+  //   var numHorses = horses.length;
+  //   horses.map(function(horses){
+  //     console.log(horses.name + " Thanks for brushing me!1!!!! ");
+  //   });
+  //   console.log("Dun with the brushin");
+  // }
+
+  // brush(stable.horses);
+//
+//For Each Loop
+
+//   [2, 7, 9].forEach(function(element, index){
+//     // console.log(element);
+//     console.log(element + " " + index);
+//   })
+//
+//For In  use sparingly!
+
+    [2, 7, 9].forEach(function(element, index){
+      // console.log(element);
+      console.log(element + " " + index);
+    })
+
+    var farm = {
+      type: 'corn',
+      address: 'My Street'
+    };
+
+    for (prop in farm) {
+      console.log(prop + ': ' + farm[prop]);
+    }
